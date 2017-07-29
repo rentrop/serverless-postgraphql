89a5c55411e7fbce723666791a6ecbd69aa49a08
Brian Smith <briaguya@gmail.com>
2017-07-29 17:50:46 -0400

    Add react storybook for testing frontend components
    In order to provide a better testing framework and sandbox environment for testing frontend components as we build them, I added react storybook and storyshots. I then set up a few example components within storybook.

---
+++ frontend/src/AdminCrossingDescriptionCell.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class AdminCrossingDescriptionCell extends React.Component {
+
+  render () {
+    return (
+      <div className='pa3 bg-black-05 ma3'>
+        <div className='pt3'>
+          {this.props.crossing.name}&nbsp;
+        </div>
+        <div className='pt3'>
+          User Readable Address
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AdminCrossingDescriptionCell;
---
+++ frontend/src/AdminCrossingStatusCautionEditFields.js
@@ -0,0 +1,12 @@
+class AdminCrossingStatusCautionEditFields extends React.Component {
+
+  render () {
+    return (
+      <div className='pa3 bg-black-05 ma3'>
+        <input type="text" placeholder="Notes"/>
+      </div>
+    );
+  }
+}
+
+export default AdminCrossingStatusCautionEditFields;
