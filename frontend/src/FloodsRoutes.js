4d1986014a26b78e235b52be514bfeb8ee352fd8
Mateo Clarke <code@mateoclarke.com>
2017-09-07 10:12:29 -0500

    Update Routing for Root Path
    There are two main things going on here. 1) I added logic to skip the GraphQL query when the user is not authed. 2) I set up a placeholder public homepage and routing for us to come back and fill in.

--- frontend/src/FloodsRoutes.js
+++ frontend/src/FloodsRoutes.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import Login from './Login';
 import PrivateRoute from './PrivateRoute';
 import Header from './Dashboard/Header/Header';
 import ManageUsers from './Dashboard/ManageUsersPage/ManageUsers';
@@ -7,6 +8,7 @@ import CreateUser from './CreateUser';
 import CrossingUpdates from './Dashboard/CrossingUpdatesPage/CrossingUpdates'
 import CrossingMap from './Map/CrossingMap';
 import NewStatusUpdate from './NewStatusUpdate';
+import PublicHomepage from './PublicHomepage';
 import auth from './services/gqlAuth';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
@@ -14,25 +16,27 @@ import gql from 'graphql-tag';
 class FloodsRoutes extends Component {
   render() {
     const currentUser = this.props.data && this.props.data.currentUser;
-
+    
     if (this.props.data && this.props.data.loading) {
       return (<div>Loading</div>)
     }
-
-    return (
-        <div>
-          <Route path="/dashboard" render={(props) => <Header currentUser={currentUser} {...props} />} />
-          <PrivateRoute path="/dashboard/users" component={ManageUsers}
-            authenticated={auth.isAuthenticated()}
-            authorized={auth.roleAuthorized(['floods_community_admin', 'floods_super_admin'])}
-            currentUser={currentUser}
-          />
-          <PrivateRoute path="/dashboard/crossings" component={CrossingUpdates}
-            authenticated={auth.isAuthenticated()}
-            authorized={auth.roleAuthorized(['floods_community_editor','floods_community_admin', 'floods_super_admin'])}
-            currentUser={currentUser}
-          />
-        </div>
+              
+    <div>
+      <Route path="/" exact component={PublicHomepage} />
+      <Route path="/dashboard" render={(props) => <Header currentUser={currentUser} {...props} />} />
+      <Route path="/dashboard/map" component={CrossingMap} currentUser={currentUser}/>
+      <Route path="/login" component={Login}/>
+      <PrivateRoute path="/dashboard/users" component={ManageUsers}
+        authenticated={auth.isAuthenticated()}
+        authorized={auth.roleAuthorized(['floods_community_admin', 'floods_super_admin'])}
+        currentUser={currentUser}
+      />
+      <PrivateRoute path="/dashboard/crossings" component={CrossingUpdates}
+        authenticated={auth.isAuthenticated()}
+        authorized={auth.roleAuthorized(['floods_community_editor','floods_community_admin', 'floods_super_admin'])}
+        currentUser={currentUser}
+      />
+    </div>
     );
   }
 }
---
+++ frontend/src/PublicHomepage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class PublicHomepage extends Component {
+
+  render() {
+    return (
+      <div>
+        <h2>Placeholder for Public Homepage</h2>
+        <p><Link to="/login">Login</Link></p>
+        <p><Link to="/dashboard">Admin Dashboard</Link></p>
+      </div>
+
+    );
+  }
+
+}
+
+export default PublicHomepage;
