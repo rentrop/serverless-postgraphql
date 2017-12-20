6f7866e661e0549cdac8ddcae1b568b3ec0233aa
Brian Smith <briaguya@gmail.com>
2017-12-19 18:17:59 -0600

    little bit of sidebar stuff

--- frontend/src/components/Dashboard/CrossingMapPage/CrossingMapPage.js
+++ frontend/src/components/Dashboard/CrossingMapPage/CrossingMapPage.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
-import CrossingMap from 'components/Map/CrossingMap'; 
+import CrossingMap from 'components/Map/CrossingMap';
+import CrossingMapSidebar from 'components/Dashboard/CrossingMapPage/CrossingMapSidebar';
 import 'components/Dashboard/CrossingMapPage/CrossingMapPage.css';
 
 class CrossingMapPage extends Component {
+  constructor(props) {
+    super(props);
 
-  render() {
     const envelope = JSON.parse(this.props.currentUser.communityByCommunityId.viewportgeojson);
     const viewport = [
       [Math.min(...envelope.coordinates[0].map(arr => arr[0])), Math.min(...envelope.coordinates[0].map(arr => arr[1]))],
       [Math.max(...envelope.coordinates[0].map(arr => arr[0])), Math.max(...envelope.coordinates[0].map(arr => arr[1]))]
     ];
 
+    this.state = {
+      showSidebar: false,
+      viewport: viewport
+    };
+  }
+
+  toggleSidebar = () => {
+    this.setState({showSidebar: !this.state.showSidebar});
+  }
+
+  render() {
+    const { showSidebar, viewport } = this.state;
+
     return (
-      <div className="CrossingMapPage">
-        <div className="CrossingMapPage__map-container">
-          <CrossingMap mapHeight="80vh" mapWidth="100%" viewport={viewport}/>
+      <div>
+        <div onClick={this.toggleSidebar}> toggle sidebar </div>
+        <div className="CrossingMapPage">
+          {showSidebar ? <CrossingMapSidebar/> : null}
+          <div className="CrossingMapPage__map-container">
+            <CrossingMap mapHeight="80vh" mapWidth="100%" viewport={viewport}/>
+          </div>
         </div>
       </div>
     );
@@ -22,3 +41,5 @@ class CrossingMapPage extends Component {
 }
 
 export default CrossingMapPage;
+
+
---
+++ frontend/src/components/Dashboard/CrossingMapPage/CrossingMapSidebar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import CrossingMap from 'components/Map/CrossingMap'; 
+import 'components/Dashboard/CrossingMapPage/CrossingMapPage.css';
+
+class CrossingMapSidebar extends Component {
+
+  render() {
+    return (
+      <div className="CrossingMapSidebar">
+        BLARG
+      </div>
+    );
+  }
+}
+
+export default CrossingMapSidebar;
