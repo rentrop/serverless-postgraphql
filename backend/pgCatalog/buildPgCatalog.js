749c9914d017c40bdcded2f4ba7b4a2e393f6ada
Luqmaan Dawoodjee <luqmaan@ordoro.com>
2018-02-26 16:04:11 -0600

    Forgot to run prettier on these files (#137)

--- backend/pgCatalog/buildPgCatalog.js
+++ backend/pgCatalog/buildPgCatalog.js
@@ -5,19 +5,27 @@ const minify = require('pg-minify');
 var client = new Client(process.argv[2]);
 client.connect();
 
-fs.readFile('./node_modules/postgraphql/resources/introspection-query.sql', (err,data) => {
-  client.query({
-    name: 'introspectionQuery',
-    text: minify(data.toString()),
-    values: [[process.argv[3]]]
-  })
-  .then(res => {
-    const out = res.rows.map(function (x) { return(x.object); });
-    fs.writeFile('./pgCatalog/pgCatalog.json', JSON.stringify(out), (err) => {
-      if (err) { console.log(err); }
-      console.log('Wrote pgCatalog to /pgCatalog/pgCatalog.json')
-    })
-  })
-  .then(() => client.end())
-  .catch(err => console.log(err))
-})
+fs.readFile(
+  './node_modules/postgraphql/resources/introspection-query.sql',
+  (err, data) => {
+    client
+      .query({
+        name: 'introspectionQuery',
+        text: minify(data.toString()),
+        values: [[process.argv[3]]],
+      })
+      .then(res => {
+        const out = res.rows.map(function(x) {
+          return x.object;
+        });
+        fs.writeFile('./pgCatalog/pgCatalog.json', JSON.stringify(out), err => {
+          if (err) {
+            console.log(err);
+          }
+          console.log('Wrote pgCatalog to /pgCatalog/pgCatalog.json');
+        });
+      })
+      .then(() => client.end())
+      .catch(err => console.log(err));
+  },
+);
