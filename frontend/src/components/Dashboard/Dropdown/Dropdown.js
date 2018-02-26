eaca23fa77c18c84740f5552a68cf9361b2a902f
Luqmaan Dawoodjee <luqmaan@ordoro.com>
2018-02-26 13:52:01 -0600

    Match CrossingListItem more closely to Invision (#129)
    #126
    
    * Match status toggle more closely to invision
    
    * Match crossinglist item components more closely
    
    * Update tests

--- frontend/src/components/Dashboard/Dropdown/Dropdown.js
+++ frontend/src/components/Dashboard/Dropdown/Dropdown.js
@@ -1,29 +1,33 @@
-import React, { Component } from 'react';
-import 'components/Dashboard/Dropdown/Dropdown.css'
+import React, { Component } from "react";
+import "components/Dashboard/Dropdown/Dropdown.css";
+
+import FontAwesome from "react-fontawesome";
 
 class Dropdown extends Component {
-
   render() {
     const { selected, options } = this.props;
 
     return (
       <div className="Dropdown">
-        <select 
+        <select
           className="Dropdown__select"
           name=""
           id=""
           onChange={this.props.onChange}
-          defaultValue={selected}>
-          {options.map(opt => 
+          defaultValue={selected}
+        >
+          {options.map(opt => (
             <option value={opt.id} key={opt.id}>
               {opt.name}
             </option>
-          )}
+          ))}
         </select>
+        <div className="Chevron">
+          <FontAwesome name="chevron-down" />
+        </div>
       </div>
     );
   }
-
 }
 
 export default Dropdown;
