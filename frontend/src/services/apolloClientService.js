d092bc5fcfeb524ca6e5b780ecb2d3f501e1e2b8
Luqmaan <ldawoodjee@gmail.com>
2018-03-01 10:29:00 -0600

    Cleanup

--- frontend/src/services/apolloClientService.js
+++ frontend/src/services/apolloClientService.js
@@ -20,30 +20,8 @@ const jwtMiddleware = new ApolloLink((operation, forward) => {
     });
   }
 
-
   return forward(operation);
 });
-//
-// httpLink.use([
-//   {
-//     applyMiddleware(req, next) {
-//       if (!req.options.headers) {
-//         req.options.headers = {}; // Create the header object if needed.
-//       }
-//
-//       var token = localStorage.getItem('jwt_user_token');
-//
-//       if (token !== null && token !== 'null') {
-//         if (!isTokenExpired(token)) {
-//           req.options.headers.Authorization = `Bearer ${token}`;
-//         } else {
-//           req.options.headers = {};
-//         }
-//       }
-//       next();
-//     },
-//   },
-// ]);
 
 const client = new ApolloClient({
   link: concat(jwtMiddleware, httpLink),
